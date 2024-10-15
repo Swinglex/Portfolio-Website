f0a26fd2e4af014b08f7b1b216d836d89ef2a081
Swinglex <153555916+Swinglex@users.noreply.github.com>
2024-10-15 10:48:21 -0500

    Front-End Overhaul

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
-const nav = document.querySelector('nav');
+window.onload = function(){
+
+
+    if(!sessionStorage.getItem('siteAlreadyLoaded')) {
+        const bodyTag = document.querySelectorAll('body');
+        
+        let animation = anime({
+            targets: bodyTag,
+            scale: [
+                {value: 0, easing: 'easeOutSine', duration: -100},
+                {value: 1, easing: 'easeInOutQuad', duration: 2000}
+              ],
+              delay: anime.stagger(0, {grid: [1, 10], from: 'center'})
+        })
+        
+        
+    }
+
+    sessionStorage.setItem('siteAlreadyLoaded', 'true')
+
+
   
-var animation = anime({
-    targets: nav, 
-    opacity: [0, 1], 
-    duration: 2000, 
-    easing: 'linear', 
-    direction: 'alternate', 
-    loop: false
-    });   
+
+    
+   
+}
